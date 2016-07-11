a580b03bae02327013ddc2f798007eb18c2a0838
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 16:50:01 -0700

    Reads file and prints restaurants and ratings in alphabetical order

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,1 +1,28 @@
 # your code goes here
+
+def restaurant_ratings(path):
+    """Reads data file and displays scores.  
+
+    Returns list of restaurants and ratings in albathetical order.
+    """
+    score_data = open(path)
+    restaurant_ratings_dicts = {}
+
+    for line in score_data:
+        line = line.rstrip()
+        restaurant_data = line.split(":")
+        restaurant_name, rating = restaurant_data
+        restaurant_ratings_dicts[restaurant_name] = rating
+
+    sorted_restaurants = sorted(restaurant_ratings_dicts)
+
+    for restaurants in sorted_restaurants:
+        print "%s is rated at %s." % (restaurants, restaurant_ratings_dicts[restaurants])
+  
+    score_data.close()
+
+    return None 
+
+restaurant_ratings("scores.txt")
+
+#unordered dictionary 
